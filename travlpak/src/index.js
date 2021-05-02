bae055ea830bf49613bb1340a0a472721fec204d
Hannah May <53949359+hfmay@users.noreply.github.com>
2021-05-01 22:57:48 -0700

    Update index.js

--- travlpak/src/index.js
+++ travlpak/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from "react-dom";
 import "./toBeDeleted/index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-<<<<<<< Updated upstream
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Register />
-=======
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
->>>>>>> Stashed changes
   </React.StrictMode>,
   document.getElementById("root")
 );
