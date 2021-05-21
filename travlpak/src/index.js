c1ca6b0fe34f948bdfa8d1448b82c71b29a3e2a9
Jose Antonio Ortega Jr <josea.ortegaa98@gmail.com>
2021-05-20 17:08:38 -0700

    Adding calendar component for plan page

--- travlpak/src/index.js
+++ travlpak/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./toBeDeleted/index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import Calendar from "./components/Calendar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calendar />
   </React.StrictMode>,
   document.getElementById("root")
 );
