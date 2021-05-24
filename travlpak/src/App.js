759bf8e3761c86dc5b9efddb04517a97b889ba75
Jose Antonio Ortega Jr <josea.ortegaa98@gmail.com>
2021-05-24 11:46:18 -0700

    Adding route to calendar on home page

--- travlpak/src/App.js
+++ travlpak/src/App.js
@@ -1,5 +1,5 @@
 import "./toBeDeleted/App.css";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
@@ -18,34 +18,38 @@ import Profile from "./components/Profile";
 import Plan from "./components/Plan";
 import NewTrip from "./components/NewTrip";
 import TripOverview from "./components/TripOverview";
+import Calendar from "./components/Calendar";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={LogIn}/>
-        <Route path="/login" component={LogIn}/>
-        <Route path="/signup" component={SignUp}/>
-        <Route path="/success" component={RegisterSuccess}/>
-        <Route path="/createProfile" component={CreateProfile}/>
-        <Route path="/home" component={Home}/>
-        <Route path="/savedLocations-mapView" exact component={SavedLocationsMapView}/>
-        <Route path="/searchPreferences" component={SearchPreferences}/>
-        <Route path="/accountSettings" component={AccountSettings}/>
-        <Route path="/explore" component={Explore}/>
-        <Route path="/connect" component={Connect}/>
-        <Route path="/messageCenterFriends" component={MessageCenter}/>
-        <Route path="/messageCenterTrips" component={MessageCenterTrips}/>
-        <Route path="/chat" component={Chat}/>
-        <Route path="/profile" component={Profile}/>
-        <Route path="/plan" component={Plan}/>
-        <Route path="/tripOverview" component={TripOverview}/>
-        <Route path="/newTrip" component={NewTrip}/>
+        <Route path="/" exact component={LogIn} />
+        <Route path="/login" component={LogIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/success" component={RegisterSuccess} />
+        <Route path="/createProfile" component={CreateProfile} />
+        <Route path="/home" component={Home} />
+        <Route
+          path="/savedLocations-mapView"
+          exact
+          component={SavedLocationsMapView}
+        />
+        <Route path="/searchPreferences" component={SearchPreferences} />
+        <Route path="/accountSettings" component={AccountSettings} />
+        <Route path="/explore" component={Explore} />
+        <Route path="/connect" component={Connect} />
+        <Route path="/messageCenterFriends" component={MessageCenter} />
+        <Route path="/messageCenterTrips" component={MessageCenterTrips} />
+        <Route path="/chat" component={Chat} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/plan" component={Plan} />
+        <Route path="/tripOverview" component={TripOverview} />
+        <Route path="/newTrip" component={NewTrip} />
+        <Route path="/calendar" component={Calendar} />
       </Switch>
     </Router>
   );
 }
 
 export default App;
-
-
